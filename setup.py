604a3decef663daad558dc8c14c4b9e458244e43
Arne Küderle <a.kuederle@gmail.com>
2019-03-15 11:18:58 +0100

    Made installable

---
+++ setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf8 -*-
+import os
+
+from setuptools import setup, find_packages
+
+# Meta information
+dirname = os.path.dirname(__file__)
+
+setup(
+    # Basic info
+    name='imucal',
+    description='Convenience methods to calibrate IMUs.',
+    long_description=open('README.md').read(),
+    classifiers=[
+    ],
+
+    packages=find_packages('imucal'),
+    install_requires=[
+        'pandas',
+        'numpy'
+    ],
+)
