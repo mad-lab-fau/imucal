e121e8f6bdbd66b4596c0d078568ceed4da4eb05
Arne Küderle <a.kuederle@gmail.com>
2019-03-15 13:30:00 +0100

    Added new import and export options to calibration

--- imucal/calibration_info.py
+++ imucal/calibration_info.py
@@ -1,67 +1,69 @@
+import json
+from collections import namedtuple
+
 import h5py
 import numpy as np
 
+_calibration_info = namedtuple('CalibrationInfo', ['K_a', 'R_a', 'b_a', 'K_g', 'R_g', 'K_ga', 'b_g'])
 
-class CalibrationInfo:
 
-    def __init__(self, K_a, R_a, b_a, K_g, R_g, K_ga, b_g):
-        """
-        Contructor for calibration matrices class
-        :param K_a: Scaling matrix acceleration (numpy array)
-        :param R_a: Rotation matrix acceleration (numpy array)
-        :param b_a: Bias vector acceleration (numpy array)
-        :param K_g: Scaling matrix gyroscope (numpy array)
-        :param R_g: Rotation matrix gyroscope (numpy array)
-        :param K_ga: Effect matrix of acceleration on gyroscope (numpy array)
-        :param b_g: Bias vector gyroscope (numpy array)
-        """
-
-        self.K_a = K_a
-        self.R_a = R_a
-        self.b_a = b_a
-        self.K_g = K_g
-        self.R_g = R_g
-        self.K_ga = K_ga
-        self.b_g = b_g
+class CalibrationInfo(_calibration_info):
+    __slots__ = ()
 
     def __repr__(self):
-        out = ''
-        for val in ['K_a', 'R_a', 'b_a', 'K_g', 'R_g', 'K_ga', 'b_g']:
-            out += '\n\n' + val + ':\n' + str(getattr(self, val))
+        out = 'CalibrationInfo('
+        for val in self._fields:
+            out += '\n' + val + ' =\n' + getattr(self, val).__repr__() + ',\n'
+        out += '\n)'
         return out
 
-    def save_to_hdf5(self, filename):
+    def _to_list_dict(self):
+        return {key: getattr(self, key).tolist() for key in self._fields}
+
+    def to_hdf5(self, filename):
         """
         Saves calibration matrices to hdf5 fileformat
         :param filename: filename (including h5 at end)
         """
 
         with h5py.File(filename, 'w') as hdf:
-            hdf.create_dataset('K_a', data=self.K_a)
-            hdf.create_dataset('R_a', data=self.K_a)
-            hdf.create_dataset('b_a', data=self.b_a)
-            hdf.create_dataset('K_g', data=self.K_g)
-            hdf.create_dataset('R_g', data=self.R_g)
-            hdf.create_dataset('K_ga', data=self.K_ga)
-            hdf.create_dataset('b_g', data=self.b_g)
+            for k, v in self._asdict().iteritems():
+                hdf.create_dataset(k, v)
 
+    def to_json(self):
+        data_dict = self._to_list_dict()
+        return json.dumps(data_dict, indent=4)
 
-def read_from_hdf5(filename):
-    """
-    Reads calibration data stored in hdf5 fileformat (created by CalibrationInfo save_to_hdf5)
-    :param filename: filename
-    :return: CalibrationInfo object
-    """
+    def to_json_file(self, path):
+        data_dict = self._to_list_dict()
+        return json.dump(data_dict, open(path, 'w'))
 
-    with h5py.File(filename, 'r') as hdf:
-        K_a = np.array(hdf.get('K_a'))
-        R_a = np.array(hdf.get('R_a'))
-        b_a = np.array(hdf.get('b_a'))
-        K_g = np.array(hdf.get('K_g'))
-        R_g = np.array(hdf.get('R_g'))
-        K_ga = np.array(hdf.get('K_ga'))
-        b_g = np.array(hdf.get('b_g'))
+    @classmethod
+    def from_hdf5(cls, path):
+        """
+        Reads calibration data stored in hdf5 fileformat (created by CalibrationInfo save_to_hdf5)
+        :param filename: filename
+        :return: CalibrationInfo object
+        """
 
-        calib_mat = CalibrationInfo(K_a, R_a, b_a, K_g, R_g, K_ga, b_g)
+        with h5py.File(path, 'r') as hdf:
+            values = dict()
+            for k in cls._fields:
+                values[k] = np.array(hdf.get(k))
 
-        return calib_mat
+        return cls(**values)
+
+    @classmethod
+    def _from_list_dict(cls, list_dict):
+        raw_json = {k: np.array(v) for k, v in list_dict.items()}
+        return cls(**raw_json)
+
+    @classmethod
+    def from_json(cls, json_str):
+        raw_json = json.loads(json_str)
+        return cls._from_list_dict(raw_json)
+
+    @classmethod
+    def from_json_file(cls, path):
+        raw_json = json.load(open(path, 'r'))
+        return cls._from_list_dict(raw_json)
