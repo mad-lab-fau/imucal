d30fda7c16d24e5e4092ebd795ed83cd49936171
Arne Küderle <a.kuederle@gmail.com>
2019-03-17 15:44:28 +0100

    First version of reimplemented algo

--- imucal/calibration.py
+++ imucal/calibration.py
@@ -1,8 +1,163 @@
+from typing import Optional
+
 import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd
 from numpy.linalg import inv
 from imucal import calibration_info as cm
+from imucal.calibration_info import CalibrationInfo
+
+
+class Calibration:
+    acc_x_p: np.ndarray
+    acc_x_a: np.ndarray
+    acc_y_p: np.ndarray
+    acc_y_a: np.ndarray
+    acc_z_p: np.ndarray
+    acc_z_a: np.ndarray
+    gyr_x_p: np.ndarray
+    gyr_x_a: np.ndarray
+    gyr_y_p: np.ndarray
+    gyr_y_a: np.ndarray
+    gyr_z_p: np.ndarray
+    gyr_z_a: np.ndarray
+
+    acc_x_rot: np.ndarray
+    acc_y_rot: np.ndarray
+    acc_z_rot: np.ndarray
+    gyr_x_rot: np.ndarray
+    gyr_y_rot: np.ndarray
+    gyr_z_rot: np.ndarray
+
+    _fields = (
+        'acc_x_p', 'acc_x_a', 'acc_y_p', 'acc_y_a', 'acc_z_p', 'acc_z_a', 'gyr_x_p', 'gyr_x_a', 'gyr_y_p', 'gyr_y_a',
+        'gyr_z_p', 'gyr_z_a', 'acc_x_rot', 'acc_y_rot', 'acc_z_rot', 'gyr_x_rot', 'gyr_y_rot', 'gyr_z_rot'
+    )
+
+    sampling_rate: float
+    grav: float
+
+    EXPECTED_ANGLE: float = 360.
+
+    def __init__(self, sampling_rate: Optional[float] = None, grav: Optional[float] = 9.81, **kwargs) -> None:
+        for field in self._fields:
+            setattr(self, field, kwargs.get(field, None))
+
+        self.sampling_rate = sampling_rate
+        self.grav = grav
+        super().__init__()
+
+    def compute_calibration_matrix(self):
+        cal_mat = CalibrationInfo()
+
+        # Note this implementation uses median instead of mean in an attempt to be robust against outlier values during
+        # calibration.
+
+        ###############################################################################################################
+        # Compute Acceleration Matrix
+
+        # Calculate means from all static phases and stack them into 3x3 matrices
+        U_a_p = np.vstack((
+            np.median(self.acc_x_p, axis=0),
+            np.median(self.acc_y_p, axis=0),
+            np.median(self.acc_y_p, axis=0),
+        ))
+        U_a_n = np.vstack((
+            np.median(self.acc_x_a, axis=0),
+            np.median(self.acc_y_a, axis=0),
+            np.median(self.acc_y_a, axis=0),
+        ))
+
+        # Eq. 19
+        U_a_s = U_a_p + U_a_n
+
+        # Bias Matrix
+        # Eq. 20
+        B_a = U_a_s / 2
+
+        # Bias Vector
+        b_a = np.diag(B_a)
+        cal_mat.b_a = b_a
+
+        # Compute Scaling and Rotation
+        # No need for bias correction, since it cancels out!
+        # Eq. 21
+        U_a_d = U_a_p - U_a_n
+
+        # Calculate Scaling matrix
+        # Eq. 23
+        k_a_sq = 1 / (4 * self.grav ** 2) * np.diag(U_a_d @ U_a_d.T)
+        K_a = np.diag(np.sqrt(k_a_sq))
+        cal_mat.K_a = K_a
+
+        # Calculate Rotation matrix
+        # Eq. 22
+        R_a = inv(K_a) @ U_a_d / (2 * self.grav)
+        cal_mat.R_a = R_a
+
+        ###############################################################################################################
+        # Calculate Gyroscope Matrix
+
+        # Gyro Bias from the static phases of the acc calibration
+        # One static phase would be sufficient, but why not use all of them if you have them.
+        # Note that this calibration ignores any influences due to the earth rotation.
+
+        b_g = np.median(np.hstack((
+            self.gyr_x_p,
+            self.gyr_x_a,
+            self.gyr_y_p,
+            self.gyr_y_a,
+            self.gyr_z_p,
+            self.gyr_z_a,
+        )), axis=0)
+        cal_mat.b_g = b_g
+
+        # Acceleration sensitivity
+
+        U_g_p = np.vstack((
+            np.median(self.gyr_x_p, axis=0),
+            np.median(self.gyr_y_p, axis=0),
+            np.median(self.gyr_y_p, axis=0),
+        ))
+        U_g_n = np.vstack((
+            np.median(self.gyr_x_a, axis=0),
+            np.median(self.gyr_y_a, axis=0),
+            np.median(self.gyr_y_a, axis=0),
+        ))
+
+        # Eq. 9
+        K_ga = (U_g_p - U_g_n) / (2 * self.grav)
+        cal_mat.K_ga = K_ga
+
+        # Gyroscope Scaling and Rotation
+
+        # First apply partial calibration to remove offset and acc influence
+        acc_x_rot_cor = cal_mat.calibrate_acc(self.acc_x_rot)
+        acc_y_rot_cor = cal_mat.calibrate_acc(self.acc_y_rot)
+        acc_z_rot_cor = cal_mat.calibrate_acc(self.acc_z_rot)
+        gyr_x_rot_cor = cal_mat._calibrate_gyro_offsets(self.gyr_x_rot, acc_x_rot_cor)
+        gyr_y_rot_cor = cal_mat._calibrate_gyro_offsets(self.gyr_y_rot, acc_y_rot_cor)
+        gyr_z_rot_cor = cal_mat._calibrate_gyro_offsets(self.gyr_z_rot, acc_z_rot_cor)
+
+        # Integrate gyro readings
+        # Eg. 13/14
+        W_s = np.zeros((3, 3))
+        W_s[0, :] = np.sum(gyr_x_rot_cor, axis=0) / self.sampling_rate
+        W_s[1, :] = np.sum(gyr_y_rot_cor, axis=0) / self.sampling_rate
+        W_s[2, :] = np.sum(gyr_z_rot_cor, axis=0) / self.sampling_rate
+
+        # Eq.15
+        expected_angles = self.EXPECTED_ANGLE * np.identity(3)
+        multiplied = expected_angles @ inv(W_s)
+
+        # Eq. 12
+        k_g_sq = np.diag(multiplied @ multiplied.T)
+        K_g = np.diag(np.sqrt(k_g_sq))
+        cal_mat.K_g = K_g
+
+        R_g = inv(K_g) @ multiplied
+        cal_mat.R_g = R_g
+
+        return cal_mat
 
 
 # Compute calibration matrix
@@ -155,7 +310,7 @@ def compute_calibration_matrix(X_p, X_a, Y_p, Y_a, Z_p, Z_a, Rot_X, Rot_Y, Rot_Z
     # -> rotation matrix
     R_g = np.matmul(inv(K_g), multiplied)
 
-    calib_mat = cm.CalibrationInfo(K_a, R_a, b_a, K_g, R_g, K_ga, b_g)
+    calib_mat = cm.CalibrationInfo(K_a=K_a, R_a=R_a, b_a=b_a, K_g=K_g, R_g=R_g, K_ga=K_ga, b_g=b_g)
 
     return calib_mat
 
@@ -310,22 +465,3 @@ def reverseCalibration(acc, gyro, calib_mat):
         # gyro_calib[i,:]=np.transpose(np.matmul(gyro_mat,(np.transpose([gyro[i,:]])-np.matmul(K_ga,np.transpose([acc_calib[i,:]]))-b_g)))
 
     return acc_reverse, gyro_reverse
-
-
-def save_calibration_data(filename, acc, gyro, calib_mat):
-    """
-    Save both uncalibrated and calibrated data to filename. Saved files contain accx, accY, accZ, gyroX, gyroY, gyroZ
-    :param filename to save, calibrated and uncalibrated will be added
-    :param acc: Acceleration x,y,z (numpy ndarray, first dimension samples, second dimension different x,y,z)
-    :param gyro: Gyroscope x,y,z (numpy ndarray, first dimension samples, second dimension different x,y,z)
-    :param calib_mat: object with calibration matrices (CalibrationInfo    """
-
-    acc_calib, gyro_calib = calibrate_array(acc, gyro, calib_mat)
-
-    test = np.concatenate((acc_calib, gyro_calib), axis=1)
-    test2 = pd.DataFrame(data=test, columns=['accX', 'accY', 'accZ', 'gyroX', 'gyroY', 'gyroZ'])
-    test2.to_csv(filename + '_calibratedData.csv', sep=';', encoding='utf-8', index=False)
-
-    test = np.concatenate((acc, gyro), axis=1)
-    test2 = pd.DataFrame(data=test, columns=['acc_x', 'acc_y', 'acc_z', 'gyro_x', 'gyro_y', 'gyro_z'])
-    test2.to_csv(filename + '_uncalibratedData.csv', sep=';', encoding='utf-8', index=False)
