dc280a25b2900089b7be9f00cb442e42cca8a227
Arne Küderle <arne.kuederle@fau.de>
2020-12-29 11:15:50 +0100

    Fixed bug with help button in label gui

--- tests/_test_gui.py
+++ tests/_test_gui.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 from imucal import ferraris_regions_from_interactive_plot
 
-data = pd.read_csv(Path(__file__).parent / "_test_data/example_data.csv")
+data = pd.read_csv(Path(__file__).parent / "_test_data/example_data.csv").sort_values("samples")
 
 section_data, section_list = ferraris_regions_from_interactive_plot(
     data, acc_cols=["accX", "accY", "accZ"], gyr_cols=["gyroX", "gyroY", "gyroZ"]
