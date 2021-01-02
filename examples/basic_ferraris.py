2124212becce20cbeec4e36d9617b70331710a79
Arne Küderle <arne.kuederle@fau.de>
2021-01-02 18:14:18 +0100

    Added actual examples

---
+++ examples/basic_ferraris.py
@@ -0,0 +1,10 @@
+r"""
+.. _basic_ferraris:
+
+Annotate a session and perform a Ferraris Calibration
+=====================================================
+"""
+
+from imucal import FerrarisCalibrationInfo
+
+FerrarisCalibrationInfo()
