6cf30e26bdc965bee7112806a76cfb21de2d0c75
Arne Küderle <arne.kuederle@fau.de>
2021-01-09 13:58:40 +0100

    Added test for version error

--- tests/test_legacy.py
+++ tests/test_legacy.py
@@ -4,6 +4,7 @@ import pytest
 
 from imucal import FerrarisCalibrationInfo
 from imucal.legacy import load_v1_json_files, load_v1_json
+from imucal.management import load_calibration_info
 
 
 @pytest.fixture()
@@ -27,3 +28,10 @@ def test_legacy_json_str(legacy_pre_2_0_cal):
     # Just check a couple of parameters to confirm that the file is loaded.
     assert isinstance(cal_info, FerrarisCalibrationInfo)
     assert cal_info.b_g[0] == -9.824970828471413
+
+
+def test_error_raised(legacy_pre_2_0_cal):
+    with pytest.raises(ValueError) as e:
+        load_calibration_info(legacy_pre_2_0_cal)
+
+    assert "`imucal.legacy`" in str(e)
