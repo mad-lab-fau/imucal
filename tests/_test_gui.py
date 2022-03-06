507ef416ef8f50e63de06e517936f1c97318dfd4
Arne Küderle <arne.kuederle@fau.de>
2022-03-06 14:41:20 +0100

    Fixed file path

--- tests/_test_gui.py
+++ tests/_test_gui.py
@@ -4,10 +4,10 @@ import pandas as pd
 
 from imucal import ferraris_regions_from_interactive_plot
 
-data = pd.read_csv(Path(__file__).parent / "_test_data/example_data.csv").sort_values("samples")
+data = pd.read_csv(Path(__file__).parent.parent / "example_data/example_ferraris_session.csv").sort_values("n_samples")
 
 section_data, section_list = ferraris_regions_from_interactive_plot(
-    data, acc_cols=["accX", "accY", "accZ"], gyr_cols=["gyroX", "gyroY", "gyroZ"]
+    data
 )
 
 print(section_data, section_list)
